36db15fbf852df8d475d0810fff5a31e6aec23f7
pikolinian <pikolinian@gazeta.pl>
2021-05-24 20:25:55 +0200

    day 6 finished

--- src/adv2015/day6.clj
+++ src/adv2015/day6.clj
@@ -1,34 +1,57 @@
 (ns adv2015.day6
   (:gen-class))
-  
-  
-  (defn make-board [size]
+    
+  (defn make-board 
+  "makes square board filled with 0's"
+  [size]
 	(vec (repeat size (vec (repeat size 0))))
   )
   
-   (defn set-on [board xy-vec]
+   (defn set-on "for day 6 first part" [board xy-vec]
 	(assoc-in board xy-vec 1)
   )
   
-   (defn set-off [board xy-vec]
+   (defn set-off "for day 6 first part" [board xy-vec]
 	(assoc-in board xy-vec 0)
   )
   
-  (defn toggle [board xy-vec]
+  (defn toggle "for day 6 first part" [board xy-vec]
 	(let [new-value (if (= 0 (get-in board xy-vec)) 1 0)]
 	(assoc-in board xy-vec new-value)
   ))
     
+	(defn incr "for day 6 sec part" [amount board xy-vec ]
+		(let [new-value (+ amount (get-in board xy-vec))]
+			(assoc-in board xy-vec new-value))
+	)
+	
+	(defn decr1 "for day 6 sec part" [board xy-vec]
+		(let [ old-value (get-in board xy-vec)]
+				(if (< 0 old-value) (assoc-in board xy-vec (dec old-value)) board))
+	)
+	
+	(defn func-part1 [line]
+		(cond 
+			(= (get line 6) \n) set-on
+			(= (get line 6) \f) set-off
+			:else toggle)
+	)
+	
+	(defn func-part2 [line]
+		(cond 
+			(= (get line 6) \n) (partial incr 1)
+			(= (get line 6) \f) decr1
+			:else (partial incr 2))
+	)
+	
   (defn make-vectors [x1 y1 x2 y2]
 	(for [x (range x1 (inc x2))
 		  y (range y1 (inc y2))]
-		[x y]	
+		[y x]	
   ))
   
-  (defn proc-board [board-arg line]
-	(loop  [fun (cond (= (get line 6) \n) set-on
-				      (= (get line 6) \f) set-off
-					  :else toggle)
+  (defn proc-board [fun-arg board-arg line]
+	(loop  [fun (fun-arg line)
 		    board board-arg			  
 			vctrs (apply make-vectors (map #( Integer/parseInt  %) (re-seq #"\d+" line)))]
 			
@@ -37,28 +60,21 @@
 	    (recur fun (fun board (first vctrs)) (rest vctrs))
   )))
   
- (defn lights-fun [fpath]
-  (loop [board (make-board 1000)
-		lines (clojure.string/split-lines (slurp fpath))]
+ (defn lights-fun [ fun lines-arg bsize]
+  (loop [board (make-board bsize)
+		lines lines-arg]
 		(if (empty? lines)
 			board 
-			(recur (proc-board board (first lines)) (rest lines))) 
+			(recur (proc-board fun board (first lines)) (rest lines))) 
 	))
  
   (defn get-answer [fpath]
-	(get (frequencies (flatten (lights-fun fpath))) 1)
-  )
- 
-  (defn lights-fun2 [fpath]
-  (loop [board (make-board 1000)
-		lines (clojure.string/split-lines (slurp fpath))]
-		
-	)) 
-  
+	(let [lines  (clojure.string/split-lines (slurp fpath))]
+	(get (frequencies (flatten (lights-fun func-part1 lines 1000))) 1)
+  ))
   
   (defn get-answer2 [fpath]
-	(reduce + (flatten (lights-fun2 fpath)))
-  )
-  
-  
-  
+  (let [lines  (clojure.string/split-lines (slurp fpath))]
+	(reduce + (flatten (lights-fun func-part2 lines 1000)))
+  ))
+   
--- test/adv2015/day6_test.clj
+++ test/adv2015/day6_test.clj
@@ -1,3 +1,37 @@
 (ns adv2015.day6-test
   (:require [clojure.test :refer :all]
             [adv2015.day6 :refer :all])) 
+			
+(deftest day6-p1 
+	(testing "small board on"
+		(is (= 9 (get (frequencies (flatten (lights-fun func-part1 ["turn on 1,1 through 3,3"] 10))) 1 ))))
+	
+	(testing "small board on and off"
+		(let [result-map (frequencies (flatten (lights-fun func-part1 ["turn on 1,1 through 3,3" "turn off 2,2 through 4,4"] 10)))]
+			(is (= 5 (get result-map 1 )))
+			(is (= 95 (get result-map 0 )))))
+	
+	(testing "small board on and toggle"
+		(let [result-map (frequencies (flatten (lights-fun func-part1 ["turn on 1,1 through 3,3" "toggle 2,2 through 4,4"] 10)))]
+			(is (= 10 (get result-map 1 )))
+			(is (= 90 (get result-map 0 )))))
+)
+
+(deftest day6-p2 
+	(testing "small board on"
+		(is (= 9 (get (frequencies (flatten (lights-fun func-part2 ["turn on 1,1 through 3,3"] 10))) 1 )))
+		(is (= 9 (reduce + (flatten (lights-fun func-part2 ["turn on 1,1 through 3,3"] 10))))))
+	(testing "small board on twice"
+		(is (= 9 (get (frequencies (flatten (lights-fun func-part2 ["turn on 1,1 through 3,3" "turn on 1,1 through 3,3"] 10))) 2 )))
+		(is (= 18 (reduce + (flatten (lights-fun func-part2 ["turn on 1,1 through 3,3" "turn on 1,1 through 3,3"] 10))))))	
+	(testing "small board on off"
+		(is (= 5 (get (frequencies (flatten (lights-fun func-part2 ["turn on 1,1 through 3,3" "turn off 2,2 through 4,4"] 10))) 1 )))
+		(is (= 95 (get (frequencies (flatten (lights-fun func-part2 ["turn on 1,1 through 3,3" "turn off 2,2 through 4,4"] 10))) 0 ))))	
+	(testing "small board on toggle"
+		(is (= 5 (get (frequencies (flatten (lights-fun func-part2 ["turn on 1,1 through 3,3" "toggle 2,2 through 4,4"] 10))) 1 )))
+		(is (= 5 (get (frequencies (flatten (lights-fun func-part2 ["turn on 1,1 through 3,3" "toggle 2,2 through 4,4"] 10))) 2 )))
+		(is (= 86 (get (frequencies (flatten (lights-fun func-part2 ["turn on 1,1 through 3,3" "toggle 2,2 through 4,4"] 10))) 0 )))
+		(is (= 27 (reduce + (flatten (lights-fun func-part2 ["turn on 1,1 through 3,3" "toggle 2,2 through 4,4"] 10)))))
+		)	
+			
+)
